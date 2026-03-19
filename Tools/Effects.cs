942508eff7c7dc4bc7a983f806fdf9cdf4496ea1
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-19 15:03:44 +0300

    Effects

---
+++ Tools/Effects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.Tools
+{
+    public static class Effects
+    {
+        public static EffectInfo Effect(BaseCombatTargettingSO targets, EffectSO effect, int var = 0)
+        {
+            if (effect is AnimationVisualsEffect vis)
+                vis._animationTarget = targets;
+
+            if (effect is AnimationVisualsIfUnitEffect vis2)
+                vis2._animationTarget = targets;
+
+            return new()
+            {
+                effect = effect,
+                targets = targets,
+                entryVariable = var,
+            };
+        }
+
+        public static EffectInfo WithCondition(this EffectInfo inf, EffectConditionSO condition)
+        {
+            inf.condition = condition;
+
+            return inf;
+        }
+    }
+}
