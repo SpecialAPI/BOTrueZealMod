29de08dca78df6c3c046498223f1eb55caa931d0
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-19 14:15:41 +0300

    Entity IDs

---
+++ ExtendedEnums/EntityIDsE.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.ExtendedEnums
+{
+    [EnumExtension(typeof(EntityIDs))]
+    public static class EntityIDsE
+    {
+        public static EntityIDs ShellyK;
+        public static EntityIDs Formosus;
+    }
+}
