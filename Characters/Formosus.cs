6c571c3e16b271effebbfdd7e428b5a1831d2191
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-19 19:48:23 +0300

    Formosus

---
+++ Characters/Formosus.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.Characters
+{
+    public static class Formosus
+    {
+        public static void Init()
+        {
+            var ch = NewCharacter("Formosus_CH", EntityIDsE.Formosus);
+            ch.SetBasicInformation("Formosus", Pigments.Purple, "FormosusFront", "FormosusBack", "FormosusOW");
+
+            ch.RankedDataSetup(4, (rank, abRank) =>
+            {
+                var massMinDamage = RankedValue(1, 2, 2, 3);
+                var massMaxDamage = RankedValue(2, 2, 3, 4);
+                var massDmgIsRng = massMinDamage != massMaxDamage;
+                var massDmgRangeString = massDmgIsRng ? $"{massMinDamage}-{massMaxDamage}" : massMinDamage.ToString();
+                var massName = $"{RankedValue("Hold", "Sacred", "Midnight", "Eternal")} Mass";
+                var massDesc = $"Deal {massDmgRangeString} indirect damage to All enemies with status effects applied to them.\nHeal All party members with status effects an equivalent amount of health to damage dealt.";
+                var mass = AbilityBuilder.NewAbility($"Mass_{abRank}_A")
+                .SetBasicInformationCharacter(massName, massDesc, "AttackIcon_Mass")
+                .SetEffects([
+                    ..massDmgIsRng ? new List<EffectInfo>()
+                    {
+                        Effects.Effect(null, CreateScriptable<ExtraVariableForNextEffect>(), massMinDamage),
+                        Effects.Effect(Targets.OpponentsWithStatuses, CreateScriptable<RandomDamageBetweenPreviousAndEntryEffect>(x => x._indirect = true), massMaxDamage)
+                    } :
+                    [Effects.Effect(Targets.OpponentsWithStatuses, CreateScriptable<DamageEffect>(x => x._indirect = true), massMinDamage)],
+
+                    Effects.Effect(Targets.AlliesWithStatuses, CreateScriptable<HealEffect>(x => x.usePreviousExitValue = true), 1)
+                ])
+                .SetIntents(new()
+                {
+                    TargetIntent(Targets.OpponentsWithStatuses, IntentForDamage(massMaxDamage)),
+                    TargetIntent(Targets.AlliesWithStatuses, IntentForHealing(massMaxDamage * 5))
+                })
+                .SetVisuals(Animations.RingABell, Targets.OpponentsWithStatuses)
+                .CharacterAbility(Pigments.Purple, RankedValue(Pigments.Yellow, Pigments.SplitPigment(Pigments.Yellow, Pigments.Purple), Pigments.SplitPigment(Pigments.Yellow, Pigments.Purple), Pigments.SplitPigment(Pigments.Yellow, Pigments.Purple)));
+
+                var confessionStatusIncrease = RankedValue(1, 2, 2, 3);
+                var confessionDivineProtection = 1;
+                var confessionName = $"{RankedValue("False", "Sinful", "Damning", "True")} Confession";
+                var confessionDesc = $"Increase the duration of all status effects inflicted to All enemies by {confessionStatusIncrease}.\nInflict {confessionDivineProtection} Divine Protection to the opposing enemy.";
+                var confession = AbilityBuilder.NewAbility($"Confession_{abRank}_A")
+                .SetBasicInformation(confessionName, confessionDesc, "AttackIcon_Confession")
+                .SetEffects(new()
+                {
+                    Effects.Effect(Targets.OpponentsWithStatuses, CreateScriptable<IncreaseOnlyStatusEffectsEffect>(x => x.positive = false), confessionStatusIncrease),
+                    Effects.Effect(Targets.OpponentsWithStatuses, CreateScriptable<IncreaseOnlyStatusEffectsEffect>(x => x.positive = true), confessionStatusIncrease),
+                    Effects.Effect(Targets.Front, CreateScriptable<ApplyDivineProtectionEffect>(), confessionDivineProtection)
+                })
+                .SetIntents(new()
+                {
+                    TargetIntent(Targets.OpponentsWithStatuses, IntentType.Misc),
+                    TargetIntent(Targets.Front, IntentType.Status_DivineProtection),
+                })
+                .SetVisuals(Animations.Malpractice, Targets.OpponentsWithStatuses)
+                .CharacterAbility(Pigments.Purple, RankedValue(Pigments.Blue, Pigments.Blue, Pigments.SplitPigment(Pigments.Blue, Pigments.Purple), Pigments.SplitPigment(Pigments.Blue, Pigments.Purple)));
+
+                var seeDamage = RankedValue(8, 10, 12, 15);
+                var seeTargets = Targets.UnitsWithStatus(false, StatusEffectType.DivineProtection);
+                var seeName = $"{RankedValue("Holy", "Roman", "Petrine", "Apostolic")} See";
+                var seeDesc = $"Deal {seeDamage} damage to All enemies inflicted with Divine Protection.";
+                var see = AbilityBuilder.NewAbility($"See_{abRank}_A")
+                .SetBasicInformation(seeName, seeDesc, "AttackIcon_See")
+                .SetEffects(new()
+                {
+                    Effects.Effect(seeTargets, CreateScriptable<DamageEffect>(), seeDamage)
+                })
+                .SetIntents(new()
+                {
+                    TargetIntent(seeTargets, IntentForDamage(seeDamage))
+                })
+                .SetVisuals(Animations.Excommunicate, seeTargets)
+                .CharacterAbility(Pigments.Purple, Pigments.Red);
+
+                return new()
+                {
+                    rankAbilities = [mass, confession, see],
+                    health = RankedValue(11, 14, 17, 19)
+                };
+            });
+        }
+    }
+}
---
+++ CustomEffects/IncreaseOnlyStatusEffectsEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.CustomEffects
+{
+    public class IncreaseOnlyStatusEffectsEffect : EffectSO
+    {
+        public bool positive;
+
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+
+            if (entryVariable <= 0)
+                return false;
+
+            for (var i = 0; i < targets.Length; i++)
+            {
+                if (!targets[i].HasUnit)
+                    continue;
+
+                exitAmount += targets[i].Unit.IncreaseStatusEffects(entryVariable, positive);
+            }
+
+            return exitAmount > 0;
+        }
+    }
+}
