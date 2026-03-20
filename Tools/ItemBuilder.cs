7aa2223e8e8c68266101fb389d1ec0b2f900c05b
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-20 19:00:32 +0300

    Evil

--- Tools/ItemBuilder.cs
+++ Tools/ItemBuilder.cs
@@ -11,18 +11,19 @@ namespace BOTrueZealMod.Tools
         private static readonly List<string> shopItemsToAdd = [];
         private static readonly List<string> treasuresToAdd = [];
 
-        [HarmonyPatch(typeof(ItemPoolDataBaseSO), nameof(ItemPoolDataBaseSO.ShopPool), MethodType.Getter)]
-        [HarmonyPatch(typeof(ItemPoolDataBaseSO), nameof(ItemPoolDataBaseSO.TreasurePool), MethodType.Getter)]
+        [HarmonyPatch(typeof(ItemPoolDataBaseSO), $"get_{nameof(ItemPoolDataBaseSO.ShopPool)}")]
+        [HarmonyPatch(typeof(ItemPoolDataBaseSO), $"get_{nameof(ItemPoolDataBaseSO.TreasurePool)}")]
+        [HarmonyPatch(typeof(ItemPoolDataBaseSO), nameof(ItemPoolDataBaseSO.InitializePool))]
         [HarmonyPrefix]
-        public static void AddItemsToPool(ItemPoolDataBaseSO __instance)
+        private static void AddItemsToPool(ItemPoolDataBaseSO __instance)
         {
             if (itemPool != null)
                 return;
 
             itemPool = __instance;
 
-            itemPool._TreasurePool = itemPool._TreasurePool.Concat(treasuresToAdd).ToArray();
-            itemPool._ShopPool = itemPool._ShopPool.Concat(shopItemsToAdd).ToArray();
+            itemPool._TreasurePool = itemPool._TreasurePool.AddRangeToArray([.. treasuresToAdd]);
+            itemPool._ShopPool = itemPool._ShopPool.AddRangeToArray([.. shopItemsToAdd]);
 
             treasuresToAdd.Clear();
             shopItemsToAdd.Clear();
