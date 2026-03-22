861a2a10181b6940fef532a262cf9d81cc97a539
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-22 21:54:21 +0300

    Achievement IDs and sprites

---
+++ AchievementIDs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod
+{
+    public static class AchievementIDs
+    {
+        public static readonly string ShellyQuest = "ACH_PartyMember_ShellyK";
+        public static readonly string ShellyHeavenUnlock = "ACH_Unlocked_ShellyK_Heaven";
+        public static readonly string ShellyOsmanUnlock = "ACH_Unlocked_ShellyK_Osman";
+
+        public static readonly string FormosusQuest = "ACH_PartyMember_Formosus";
+        public static readonly string FormosusHeavenUnlock = "ACH_Unlocked_Formosus_Heaven";
+        public static readonly string FormosusOsmanUnlock = "ACH_Unlocked_Formosus_Osman";
+    }
+}
