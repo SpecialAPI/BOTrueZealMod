ad0eea2bfca475e1a93532e54629b94374fe4caa
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-24 17:43:30 +0300

    Final boss unlock ids

---
+++ ExtendedEnums/UnlockableIDE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.ExtendedEnums
+{
+    [EnumExtension(typeof(UnlockableID))]
+    public static class UnlockableIDE
+    {
+        public static UnlockableID ShellyK;
+        public static UnlockableID Formosus;
+
+        public static UnlockableID ShellyK_Osman;
+        public static UnlockableID ShellyK_Heaven;
+
+        public static UnlockableID Formosus_Osman;
+        public static UnlockableID Formosus_Heaven;
+    }
+}
