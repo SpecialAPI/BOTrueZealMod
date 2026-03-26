30e740206ca3912c99e67bc44aba4a410b22dd76
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-26 13:02:07 +0300

    HasPartySpaceCombat dialogue function

---
+++ CustomDialogueCommands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod
+{
+    [HarmonyPatch]
+    public static class CustomDialogueCommands
+    {
+        public const string HasPartySpaceCombat = "TrueZeal_HasPartySpaceCombat";
+
+        [HarmonyPatch(typeof(CombatDialogueHandler), nameof(CombatDialogueHandler.Awake))]
+        [HarmonyPostfix]
+        private static void AddCombatDialogueCommands(CombatDialogueHandler __instance)
+        {
+            var runner = __instance._combatDialogueRunner;
+
+            runner.AddFunction(HasPartySpaceCombat, 0, parameters =>
+            {
+                if (infoHolder == null)
+                    return false;
+
+                if (!infoHolder.HasRunData)
+                    return false;
+
+                var runData = infoHolder.Run;
+                if (runData.PlayerDialogueData is not IPlayerDialogueData playerData)
+                    return false;
+
+                return playerData.HasCharacterSpace;
+            });
+        }
+    }
+}
