cc290dd191a0b9698296f8e354383896cef201d1
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-26 14:07:21 +0300

    More custom dialogue commands...

--- CustomDialogueCommands.cs
+++ CustomDialogueCommands.cs
@@ -7,6 +7,10 @@ namespace BOTrueZealMod
     [HarmonyPatch]
     public static class CustomDialogueCommands
     {
+        public const string UnlockCombat = "TrueZeal_UnlockCombat";
+        public const string HireCharacterCombat = "TrueZeal_HireCharacterCombat";
+        public const string SaveProgressCombat = "TrueZeal_SaveProgressCombat";
+
         public const string HasPartySpaceCombat = "TrueZeal_HasPartySpaceCombat";
 
         [HarmonyPatch(typeof(CombatDialogueHandler), nameof(CombatDialogueHandler.Awake))]
@@ -14,6 +18,29 @@ namespace BOTrueZealMod
         private static void AddCombatDialogueCommands(CombatDialogueHandler __instance)
         {
             var runner = __instance._combatDialogueRunner;
+
+            runner.AddCommandHandler(UnlockCombat, x =>
+            {
+                if (!Enum.TryParse<UnlockableID>(x[0], out var result))
+                    return;
+
+                infoHolder.UnlockableManager.TryUnlockContentFromID(result);
+            });
+            runner.AddCommandHandler(HireCharacterCombat, x =>
+            {
+                if (infoHolder == null)
+                    return;
+
+                if (!infoHolder.HasRunData)
+                    return;
+
+                var runData = infoHolder.Run;
+                runData.TryHireCharacter(x);
+            });
+            runner.AddCommandHandler(SaveProgressCombat, x =>
+            {
+                CombatManager.Instance.FullSaveGame();
+            });
 
             runner.AddFunction(HasPartySpaceCombat, 0, parameters =>
             {
