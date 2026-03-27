373a47abecb6f2560c8eeb396a4d77783cd0f483
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-27 18:09:26 +0300

    Basegame speaker changes

---
+++ BasegameSpeakerChanges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod
+{
+    public static class BasegameSpeakerChanges
+    {
+        public static void Init()
+        {
+            var antonSpeaker = LoadedAssetsHandler.GetSpeakerData("Anton_SpeakerData");
+            antonSpeaker._emotionBundles = antonSpeaker._emotionBundles.AddToArray(new()
+            {
+                emotion = "TrueZeal_Sad",
+                bundle = new()
+                {
+                    portrait = LoadSprite("AntonSad"),
+                    dialogueSound = antonSpeaker._defaultBundle.dialogueSound,
+                    bundleTextColor = antonSpeaker._defaultBundle.bundleTextColor,
+                }
+            });
+
+            var hansSpeaker = LoadedAssetsHandler.GetSpeakerData("Hans_SpeakerData");
+            hansSpeaker._emotionBundles = hansSpeaker._emotionBundles.AddToArray(new()
+            {
+                emotion = "TrueZeal_Surprised",
+                bundle = new()
+                {
+                    portrait = LoadSprite("HansSurprised"),
+                    dialogueSound = hansSpeaker._defaultBundle.dialogueSound,
+                    bundleTextColor = hansSpeaker._defaultBundle.bundleTextColor,
+                }
+            });
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -56,6 +56,7 @@ namespace BOTrueZealMod
             Bluey.Init();
             BlueyEncounters.Init();
 
+            BasegameSpeakerChanges.Init();
             Bar.Init();
         }
     }
