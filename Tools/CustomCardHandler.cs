213f5239ffc330d092981bb829561fa705431bc5
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-27 14:43:19 +0300

    CustomCardHandler

---
+++ Tools/CustomCardHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.Tools
+{
+    [HarmonyPatch]
+    public static class CustomCardHandler
+    {
+        private static readonly Dictionary<CardType, Action<ZoneBGDataBaseSO, CardInfo>> generators = [];
+
+        [HarmonyPatch(typeof(ZoneBGDataBaseSO), nameof(ZoneBGDataBaseSO.TryGenerateNewCard))]
+        [HarmonyPrefix]
+        private static bool TryGenerateCustomCard(ZoneBGDataBaseSO __instance, CardInfo info)
+        {
+            if (!generators.TryGetValue(info.cardType, out var generator))
+                return true;
+
+            generator?.Invoke(__instance, info);
+            return false;
+        }
+
+        public static void AddCardGenerator(CardType type, Action<ZoneBGDataBaseSO, CardInfo> generator)
+        {
+            generators[type] = generator;
+        }
+    }
+}
