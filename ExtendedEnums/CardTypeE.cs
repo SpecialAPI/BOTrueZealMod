a058246f0db5873287c2480a5a9209ab3c802fe5
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-27 13:50:36 +0300

    Bar CardType

---
+++ ExtendedEnums/CardTypeE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod.ExtendedEnums
+{
+    [EnumExtension(typeof(CardType))]
+    public static class CardTypeE
+    {
+        public static CardType EventBar;
+    }
+}
