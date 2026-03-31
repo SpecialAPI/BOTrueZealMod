3b87a4f9ac27b9241e0e49b29b7769572e60278d
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-31 15:54:12 +0300

    OnTurnStart_Early

---
+++ OnTurnStartEarlyTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod
+{
+    [HarmonyPatch]
+    public static class OnTurnStartEarlyTrigger
+    {
+        [HarmonyPatch(typeof(CharacterCombat), nameof(CharacterCombat.PlayerTurnStart))]
+        [HarmonyILManipulator]
+        private static void Characters(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchCallOrCallvirt<CombatManager>($"get_{nameof(CombatManager.Instance)}"), 2))
+                return;
+
+            crs.Emit(OpCodes.Ldarg_0);
+            crs.EmitStaticDelegate(CallTrigger);
+        }
+
+        [HarmonyPatch(typeof(EnemyCombat), nameof(EnemyCombat.StartTurn))]
+        [HarmonyILManipulator]
+        public static void Enemies(ILContext ctx)
+        {
+            var crs = new ILCursor(ctx);
+
+            if (!crs.JumpToNext(x => x.MatchCallOrCallvirt<CombatManager>($"get_{nameof(CombatManager.Instance)}"), 3))
+                return;
+
+            crs.Emit(OpCodes.Ldarg_0);
+            crs.EmitStaticDelegate(CallTrigger);
+        }
+
+        private static CombatManager CallTrigger(CombatManager curr, IUnit unit)
+        {
+            CombatManager.Instance.PostNotification(TriggerCallsE.OnTurnStart_Early.ToString(), unit, null);
+
+            return curr;
+        }
+    }
+}
