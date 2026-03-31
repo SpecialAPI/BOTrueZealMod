e9191371b1fa1883457fdf2dbb3b2675f94d0f3b
SpecialAPI <68025759+SpecialAPI@users.noreply.github.com>
2026-03-31 18:03:26 +0300

    ComatDialogueNodeVisitedPatch

---
+++ ComatDialogueNodeVisitedPatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOTrueZealMod
+{
+    [HarmonyPatch]
+    public static class ComatDialogueNodeVisitedPatch
+    {
+        [HarmonyPatch(typeof(CombatDialogueHandler), nameof(CombatDialogueHandler.Awake))]
+        [HarmonyPostfix]
+        private static void AddHandler(CombatDialogueHandler __instance)
+        {
+            var runner = __instance._combatDialogueRunner;
+            var listener = runner.GetOrAddComponent<EvilNodeCompleteListener>();
+
+            runner.onNodeComplete.AddListener(listener.NodeComplete);
+        }
+
+        private class EvilNodeCompleteListener : MonoBehaviour
+        {
+            public IInGameRunData dialogueData;
+
+            public void Awake()
+            {
+                if (infoHolder == null)
+                    return;
+
+                if (!infoHolder.HasRunData)
+                    return;
+
+                var runData = infoHolder.Run;
+                if (runData.inGameData is not RunInGameData gameRun)
+                    return;
+
+                dialogueData = gameRun;
+            }
+
+            public void NodeComplete(string nodeName)
+            {
+                dialogueData?.AddVisitedNode(nodeName);
+            }
+        }
+    }
+}
